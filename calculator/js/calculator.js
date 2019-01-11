5378b26cdda3bf9e5c370fece6d4c82dca6574ef
ninteball <stoppiestomper@yahoo.com>
2019-01-11 22:09:25 +0200

    Add files via upload

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -1,35 +1,56 @@
 
     let math;
     let operator;
-    let a ;
-    let b ;
+    let a = 3 ;
+    let b = 2 ; //gave numbers to a and b because i am trying to find a way so it won't return null error in html
     let userInput;
     let otherInput;
+    //let result = +document.getElementById("math1").value;
 
-    math = document.getElementById("math1");
-    operator = document.getElementById("op1");
+    //math = document.getElementById("math1").value = a && b;
+    operator = document.getElementById("op1").value = a && b;
+
+    console.log(operator);
+    //$('#math1').val();
+
+    // $('#op1').val();
+    // $('.operator').click(function(){
+    //    plus();
+    //    divide();
+    //    multiply();
+    //    minus()
+    //
+    // });
 
     function screen() {
         //note to self find a way to use input to connect userInput and otherInput
     }
 
     function plus(a,b) {
+        $('#op1').val();
+        $('.operator').click(function(){
+
         return (a + b);
-    }
+    });
 
     function minus(a,b) {
+        $('#op1').val();
+        $('.operator').click(function(){
         return (a - b);
 
-    }
+        });
 
     function multiply(a,b) {
-        console.log();
+        $('#op1').val();
+        $('.operator').click(function(){
         return (a * b);
-    }
+        });
 
     function divide(a,b) {
+        $('#op1').val();
+        $('.operator').click(function(){
         return (a / b);
-    }
+        });
     switch (math) {
         case 1:
             operator = plus(a + b);
@@ -55,3 +76,4 @@
     // }
 
 }
+    }
